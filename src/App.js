8c5a72fd423d43b9bad891d59c96af6e2656f1cb
Enes Kaan Bilir <bilireneskaan@gmail.com>
2023-04-09 20:44:18 +0300

    feat: Yarım useRef tekrar edilecek

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useRef } from "react";
+
 function App() {
-   
+
+   const renkInput = useRef()
+
+   function renkDeistir(){
+      const renk = renkInput.current.value
+
+      document.body.style.backgroundColor = renk
+   }
+
    return(
       <div>
-         App 1
+         
+         <input ref={renkInput} type="text"  />
+         <button onClick={renkDeistir}> Renk Değiştir</button>
       </div>
    )
 }
